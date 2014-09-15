bbe2124adce0ab84155ed9a856d0216b2cff69f8
namhosun <imac27@NamHosunui-iMac.local>
2014-09-15 15:05:55 +0900

    erro2

--- koonzprice_CONN.php
+++ koonzprice_CONN.php
@@ -4,7 +4,8 @@
     $pwd = "8231c34b649555d";
     $db = "koonzpriceDB";
 
-    $db_server = mysql_connect($host,$user,$pwd) or die ("error");
-    mysql_select_db($db_server) or die ("error");
+    $db_server = mysql_connect($host,$user,$pwd) or die ("error1");
+    mysql_select_db($db_server) or die ("error2");
+    mysql_close();
 
 ?>
